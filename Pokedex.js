83043606ada9daadb090411cef756be5f4204dee
Agasthya321 <90805993+Agasthya321@users.noreply.github.com>
2022-09-22 00:04:23 -0400

    Alert message for pokemon name

--- Pokedex.js
+++ Pokedex.js
@@ -1,26 +1,46 @@
 
-const pokemon = ["","Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle",
+const pokemon = ["Bulbasaur","Ivysaur","Venusaur","Charmander","Charmeleon","Charizard","Squirtle",
                 "Wartortle","Blastoise","Caterpie","Metapod","Butterfree","Weedle","Kakuna",
                 "Beedrill","Pidgey","Pidgeotto","Pidgeot","Rattata","Raticate"]
 
-const num = ["","#001","#002","#003","#004", "#005", "#006", "#007", "#008", "#009", "#010", "#010", "#012", "#013", "#014", "#015", "#016", "017", "018", "019", "020"]
+const num = ["#001","#002","#003","#004", "#005", "#006", "#007", "#008", "#009", "#010", "#011", "#012", "#013", "#014", "#015", "#016", "017", "018", "019", "020"]
 
-const pokemonType = ["","Grass/Poison","Grass/Poison","Grass/Poison","Fire","Fire","Fire/Flying",
+const pokemonType = ["Grass/Poison","Grass/Poison","Grass/Poison","Fire","Fire","Fire/Flying",
                     "Water","Water","Water","Bug","Bug","Bug/Flying","Bug/Poison","Bug/Poison",
                     "Bug/Poison","Normal/Flying","Normal/Flying","Normal/Flying","Normal","Normal"]
 
-const fastMoves = ["","Tackle / Vine Whip", "Razor Leaf / Vine Whip", "Razor Leaf / Vine Whip", "Ember / Scratch",
+const fastMoves = ["Tackle / Vine Whip", "Razor Leaf / Vine Whip", "Razor Leaf / Vine Whip", "Ember / Scratch",
  "Ember / Fire Fang", "Fire Spin / Air Slash", "Tackle / Bubble", "Bite / Water Gun", "Bite / Water Gun",
  "Tackle / Bug Bite", "Tackle / Bug Bite" , "Struggle Bug / Confusion", "Bug Bite / Poison Sting" , 
 "Bug Bite / Poison Sting" , "Infestation / Poison Jab" , "Tackle / Quick Attack" , "Wing Attack / Steel Wing",
 "Air Slash / Steel Wing" , "Tackle / Quick Attack" , "Bite / Quick Attack" , "Quick Attack / Peck"]
 
-const chargeMoves = ["","Sludge Bomb / Seed Bomb / Power Whip", "Sludge Bomb / Solar Beam / Power Whip",
+const chargeMoves = ["Sludge Bomb / Seed Bomb / Power Whip", "Sludge Bomb / Solar Beam / Power Whip",
 "Sludge Bomb / Solar Beam / Petal Blizzard", "Flame Burst / Flamethrower / Flame Charge", "Flame Burst / Flamethrower / Fire Punch" , 
 "Fire Blast / Dragon Claw / Overheat", "Aqua Jet / Aqua Tail / Water Pulse" , "Aqua Jet / Hydro Pump / Ice Beam" , 
 "Ice Beam / Hydro Pump / Flash Cannon" , "Struggle" , "Struggle" , "Psychic / Bug Buzz / Signal Beam" , "Struggle" , "Struggle",
 "Aerial Ace / Sludge Bomb / X-Scissor", "Twister / Aerial Ace / Air Cutter" , "Twister / Aerial Ace / Air Cutter" , 
 "Hurricane / Aerial Ace / Brave Bird" , "Dig / Hyper Fang / Body Slam" , "Dig / Hyper Fang / Hyper Beam" , "Aerial Ace / Drill Peck / Sky Attack"]
+
+function searchPokemonName(){
+
+    let pokemonName = document.getElementById("pokeName").value;
+    let output = "";
+    let count = 0;
+    
+    
+    for (let i = 0; i < pokemon.length; i++){
+        if(pokemon[i].toLowerCase().includes(pokemonName.toLowerCase())){
+            output += pokemon[i] + "/" + num[i] + " is type " + pokemonType[i] + " and has fast moves " + fastMoves[i] + " and charge moves " + chargeMoves[i] + "\n\n";
+            count++;
+        }
+
+        if(count > 4){
+            break;
+        }
+    }
+        alert(output);
+}
 
 
 
